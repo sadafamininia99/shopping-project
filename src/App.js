822c17bf14c11e4dcad8d45a72eb2174de6af23a
SadafAmininiiiaa <s.amininia@tapsell.ir>
2022-11-08 10:44:57 +0330

    directory folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import CategoryItem from './components/category-item/category-item.component';
-
+import Directory from './components/directory/directory.component';
 
 const App = () => {
   const categories = [
@@ -29,13 +28,8 @@ const App = () => {
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ];
-  return (
-    <div className='categories-container'>
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+
+  return <Directory categories={categories} />;
 };
 
 export default App;
